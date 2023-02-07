4e8dacb4f7534c7db2e78d46aaf4066b33cc0cea
silverstarlulu <117253988+silverstarlulu@users.noreply.github.com>
2023-02-07 17:21:38 +0900

    1st Commit_0207

---
+++ ssemibucks/src/main/java/ssemi/ssemibucks/SsemibucksApplication.java
@@ -0,0 +1,13 @@
+package ssemi.ssemibucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsemibucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsemibucksApplication.class, args);
+	}
+
+}
