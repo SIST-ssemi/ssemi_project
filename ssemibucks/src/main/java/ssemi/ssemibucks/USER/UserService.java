6ec37506058999003306a3ecd8d46e863e74a823
silverstarlulu <117253988+silverstarlulu@users.noreply.github.com>
2023-02-12 23:23:36 +0900

    User 최종 _0212

--- ssemibucks/src/main/java/ssemi/ssemibucks/USER/UserService.java
+++ ssemibucks/src/main/java/ssemi/ssemibucks/USER/UserService.java
@@ -8,41 +8,52 @@ public class UserService {
     Scanner sc = new Scanner(System.in);
 
     //로그인
-    public void loginUser() {
-        System.out.println("[Log in]");
-        System.out.print("아이디: ");
-        String uId = sc.nextLine();
-        System.out.print("비밀번호: ");
-        String pw = sc.nextLine();
+    public String loginUser() {
+        boolean flag = false;
+        User user;
 
-        User user = new User(uId, pw);
+        do {
+            System.out.println("[Log in]");
+            System.out.print("아이디: ");
+            String uId = sc.nextLine();
+            System.out.print("비밀번호: ");
+            String pw = sc.nextLine();
 
-        if (userDao.findByUser(uId)==null)
-            System.out.println("해당 아이디가 존재하지 않습니다.");
-        else if (userDao.findByUser(uId).getuId().equals(user.getuId())&&!userDao.findByUser(uId).getPw().equals(user.getPw()))
-            System.out.println("비밀번호가 일치하지 않습니다.");
-        else System.out.println(user.getuId()+"님 환영합니다.");
+            user = new User(uId, pw);
 
+            if (userDao.findByUser(uId) == null)
+                System.out.println("해당 아이디가 존재하지 않습니다.\n");
+
+            else if (userDao.findByUser(uId).getuId().equals(user.getuId()) && !userDao.findByUser(uId).getPw().equals(user.getPw()))
+                System.out.println("비밀번호가 일치하지 않습니다.\n");
+
+            else {
+                System.out.println(user.getuId() + "님 환영합니다.\n");
+                flag = true;
+            }
+        } while (!flag);
+
+        return user.getuId();
     }
 
     //회원가입
-    public void registerUser(){
+    public void registerUser() {
         System.out.println("[Sign up]");
         userDao.insertUser();
         loginUser();
     }
 
     //마이페이지
-    public void myPage(){
+    public void myPage() {
         System.out.println("[My Page]");
         userDao.selectUser();
 
-        while(true){
+        while (true) {
             System.out.println("1.정보수정\t2.회원탈퇴\t0.돌아가기");
             System.out.print("입력 >> ");
 
-            int num=Integer.parseInt(sc.nextLine());
-            switch (num){
+            int num = Integer.parseInt(sc.nextLine());
+            switch (num) {
                 case 1:
                     userDao.updateUser();
                     break;
@@ -60,14 +71,14 @@ public class UserService {
     }
 
     //회원관리
-    public void management(){
-        while (true){
+    public void management() {
+        while (true) {
             System.out.println("[관리자 페이지]");
             System.out.println("1. 회원목록확인\t2. 회원 강제탈퇴\t0. 종료");
             System.out.print("입력 >> ");
 
-            int num=Integer.parseInt(sc.nextLine());
-            switch (num){
+            int num = Integer.parseInt(sc.nextLine());
+            switch (num) {
                 case 1:
                     userDao.selectAllUser();
                     break;
