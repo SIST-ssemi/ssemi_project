bd31f53a03524d1b2f7e0a08cf3ce8c4d43898ad
silverstarlulu <117253988+silverstarlulu@users.noreply.github.com>
2023-06-07 19:05:50 +0900

    url 매핑_0607

--- ssemibucks/src/main/frontend/src/components/Navbar.js
+++ ssemibucks/src/main/frontend/src/components/Navbar.js
@@ -1,28 +1,201 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+// import React from "react";
+// import { NavLink } from "react-router-dom";
 
-function Navbar(props) {
+// function Navbar(props) {
+//   return (
+//     <div>
+//       <ul className="menu">
+//         <li>
+//           <NavLink to="/">SsemiBucks</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/product">상품목록</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/user">마이페이지</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/cart">장바구니</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/admin">어드민</NavLink>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
+
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import FreeBreakfastIcon from "@mui/icons-material/FreeBreakfast";
+
+const pages = [
+  { name: "상품목록", url: "/product" },
+  { name: "마이페이지", url: "/user" },
+  { name: "장바구니", url: "/cart" },
+  { name: "어드민", url: "/admin" },
+];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState("/");
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <div>
-      <ul className="menu">
-        <li>
-          <NavLink to="/">SsemiBucks</NavLink>
-        </li>
-        <li>
-          <NavLink to="/product">상품목록</NavLink>
-        </li>
-        <li>
-          <NavLink to="/user">마이페이지</NavLink>
-        </li>
-        <li>
-          <NavLink to="/cart">장바구니</NavLink>
-        </li>
-        <li>
-          <NavLink to="/admin">어드민</NavLink>
-        </li>
-      </ul>
-    </div>
+    <AppBar position="static" style={{ backgroundColor: "rgb(167, 193, 55)" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <FreeBreakfastIcon
+            sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+          />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            SsemiBucks
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <FreeBreakfastIcon
+            sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+          />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                href={page.url}
+                key={page.name}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
-
 export default Navbar;
